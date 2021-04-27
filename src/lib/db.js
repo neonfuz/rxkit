ea33e8272995baef2b5bbc6f52740363e02f3451
Sage Raflik <neonfuz@gmail.com>
2021-04-26 22:32:24 -0500

    Moved db to $lib/db.js

---
+++ src/lib/db.js
@@ -0,0 +1,24 @@
+import {onMount} from 'svelte';
+
+export const useDb = fn => (
+    // Browser only
+    onMount(async () => {
+        // Shims
+        window.global = window;
+        window.process = {env: {DEBUG: undefined}};
+        // Browser only imports
+        const {
+            createRxDatabase,
+            addRxPlugin,
+        } = await import('rxdb');
+        // RxDB Plugins
+        addRxPlugin(await import('pouchdb-adapter-idb'));
+        // Create DB
+        const db = await createRxDatabase({
+            name: 'rxkit',
+            adapter: 'idb',
+        });
+        // execute fn with db
+        return await fn(db);
+    })
+);
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,6 @@
 <script>
- import {onMount} from 'svelte';
- onMount(async () => {
-     window.process = {env: {DEBUG: undefined}};
-     window.global = window;
-     const { createRxDatabase, addRxPlugin } = await import('rxdb');
-     addRxPlugin(await import('pouchdb-adapter-idb'));
-     const db = await createRxDatabase({
-         name: 'sveltekitrxdb',
-         adapter: 'idb',
-     });
-     console.log(db)
- })
+ import {useDb} from '$lib/db.js';
+ useDb(db => console.log(db));
 </script>
 
 <h1>Welcome to SvelteKit</h1>
