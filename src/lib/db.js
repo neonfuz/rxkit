f0b6ac66410dd364734d22c1f1a5b2bc941efce0
Sage Raflik <neonfuz@gmail.com>
2021-04-28 13:15:22 -0500

    Made database HMR friendly

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,32 +1,38 @@
 import {onMount} from 'svelte';
 import models from '../models/';
 
-export const useDb = fn => (
-    // Browser only
-    onMount(async () => {
-        // Shims
-        window.global = window;
-        window.process = {env: {DEBUG: undefined}};
-        // Browser only imports
-        const {
-            createRxDatabase,
-            addRxPlugin,
-        } = await import('rxdb');
-        // RxDB Plugins
-        addRxPlugin(await import('pouchdb-adapter-idb'));
-//        addRxPlugin(await import('rxdb-utils/dist/observables'));
-//        addRxPlugin(await import('rxdb-utils/dist/hooks'));
-        // Create DB
-        const db = await createRxDatabase({
-            name: 'rxkit',
-            adapter: 'idb',
-        });
-        // Register models
-        await db.addCollections(await models());
-        // execute fn with db
-        return await fn(db);
+const createDb = async () => {
+    // Node shims needed for pouchdb
+    window.global = window;
+    window.process = {env: {DEBUG: undefined}};
+    // Imports are here so they don't run during SSR
+    const { createRxDatabase, addRxPlugin } = await import('rxdb');
+    // Add plugins
+    addRxPlugin(await import('pouchdb-adapter-idb'));
+    addRxPlugin(await import('rxdb-utils/dist/observables'));
+    addRxPlugin(await import('rxdb-utils/dist/hooks'));
+    // Create DB
+    const db = await createRxDatabase({
+        name: 'rxkit',
+        adapter: 'idb',
+    });
+    // Register models
+    await db.addCollections(await models());
+    return db;
+}
+
+// DB is shared singleton for now, TODO: implement a db context
+let dbPromise;
+export const useDb = fn => {
+    onMount(() => {
+        // Initialize db
+        if (!dbPromise) dbPromise = createDb();
+        // Execute user function with db
+        dbPromise.then(fn);
+        // Cleanup db for HMR
+        return () => dbPromise.then(db => db.destroy());
     })
-);
+}
 
 export const handleChange = item => e => {
     let value = e.target.value;
