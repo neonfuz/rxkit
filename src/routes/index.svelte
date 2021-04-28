bdd1b8a49e29502c5d1b0ed3d9a1c2e7346a25cd
Sage Raflik <neonfuz@gmail.com>
2021-04-28 13:26:07 -0500

    Removed testing use of useDb

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,2 @@
-<script>
- import {useDb} from '$lib/db.js';
- useDb(db => console.log(db));
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
